a5aaca1e6b64d553667637ab265a7222dc57fd70
Rana Tarek Hassan <rana.t.hassan@gmail.com>
2019-10-03 16:44:09 +0000

    Fix Taiga builder

--- JumpscaleBuilders/apps/BuilderTaiga.py
+++ JumpscaleBuilders/apps/BuilderTaiga.py
@@ -14,10 +14,10 @@ NGINX_LOG_DIR = f"/home/{TAIGA_USER}/logs"
 MEDIA_CONF_FILE = """
 from .common import *
 
-MEDIA_URL = "http://example.com/media/"
-STATIC_URL = "http://example.com/static/"
+MEDIA_URL = "http://localhost/media/"
+STATIC_URL = "http://localhost/static/"
 SITES["front"]["scheme"] = "http"
-SITES["front"]["domain"] = "example.com"
+SITES["front"]["domain"] = "localhost"
 
 SECRET_KEY = "theveryultratopsecretkey"
 
@@ -118,15 +118,18 @@ class BuilderTaiga(j.baseclasses.builder):
         self.DIR_CODE = self.tools.joinpaths(self.DIR_BUILD, "code")
         self.frontend_repo_dir = f"{self.DIR_CODE}/taiga-front-dist"
         self.backend_repo_dir = f"{self.DIR_CODE}/taiga-back"
+        self.TAIGA_USER = "taiga"
+        self.port = 4321
+        self.host = "localhost"  # TODO
+        self.protocol = "http"
 
     @builder_method()
-    def install_deps(self):
+    def install_deps(self, reset=False):
         self.system.package.update()
         self.system.package.install(PACKAGES)
-        j.builders.db.redis.install()
-        j.builders.db.psql.install()
+        j.builders.db.redis.install(reset=reset)
+        j.builders.db.psql.install(reset=reset)
         j.builders.db.psql.start()
-
         create_user_cmd = f"""
         adduser --system --quiet --shell /bin/bash --group --gecos "taiga user" {TAIGA_USER}
         adduser {TAIGA_USER} sudo
@@ -135,8 +138,8 @@ class BuilderTaiga(j.baseclasses.builder):
 
         if not self._done_check("postgresuser"):
             create_psql_user_cmd = """
-            sudo -u postgres {DIR_BIN}/createuser {TAIGA_USER}
-            sudo -u postgres {DIR_BIN}/createdb {TAIGA_USER} -O {TAIGA_USER} --encoding='utf-8' --locale=en_US.utf8 --template=template0
+            sudo -u postgres {DIR_BIN}/createuser -h localhost {TAIGA_USER}
+            sudo -u postgres {DIR_BIN}/createdb {TAIGA_USER} -h localhost -O {TAIGA_USER} --encoding='utf-8' --locale=en_US.utf8 --template=template0
             """
             self._execute(create_psql_user_cmd)
             self._done_set("postgresuser")
@@ -146,47 +149,52 @@ class BuilderTaiga(j.baseclasses.builder):
         j.clients.git.pullGitRepo("https://github.com/taigaio/taiga-back.git", self.backend_repo_dir, branch="stable")
         command = f"""
         chown -R {TAIGA_USER} {self.backend_repo_dir}
-        su {TAIGA_USER} -c '
+        su - {TAIGA_USER} -c '
         cd {self.backend_repo_dir}
         source /usr/share/virtualenvwrapper/virtualenvwrapper_lazy.sh
         mkvirtualenv -p /usr/bin/python3 taiga
-        pip install -r requirements.txt
-        python manage.py migrate --noinput
-        python manage.py loaddata initial_user
-        python manage.py loaddata initial_project_templates
-        python manage.py compilemessages
-        python manage.py collectstatic --noinput
+        pip3 install -r requirements.txt
+        python3 manage.py migrate --noinput
+        python3 manage.py loaddata initial_user
+        python3 manage.py loaddata initial_project_templates
+        python3 manage.py compilemessages
+        python3 manage.py collectstatic --noinput
         '
         """
         self._execute(command)
         j.sal.fs.writeFile(f"{self.backend_repo_dir}/settings/local.py", MEDIA_CONF_FILE)
 
     @builder_method()
-    def frontend_install(self, host, port, protocol):
+    def frontend_install(self):
         j.clients.git.pullGitRepo(
             "https://github.com/taigaio/taiga-front-dist.git", self.frontend_repo_dir, branch="stable"
         )
         conf_dict = j.data.serializers.json.load(f"{self.frontend_repo_dir}/dist/conf.example.json")
-        conf_dict["api"] = f"{protocol}://{host}:{port}/api/v1/"
+        conf_dict["api"] = f"{self.protocol}://{self.host}:{self.port}/api/v1/"
         j.data.serializers.json.dump(f"{self.frontend_repo_dir}/dist/conf.json", conf_dict)
 
     @builder_method()
-    def install(self, host, port, protocol="http"):
-        self.install_deps()
+    def install(self, reset=True):
+        self.install_deps(reset=reset)
         self.backend_install()
         j.sal.fs.createDir(NGINX_LOG_DIR)
-        self.frontend_install(host, port, protocol)
+        self.frontend_install()
+
+    # @builder_method()
+    def start(self):
         j.sal.fs.writeFile(
             "/etc/nginx/conf.d/taiga.conf",
             NGINX_CONF
             % {
-                "port": port,
+                "port": self.port,
                 "log_dir": NGINX_LOG_DIR,
                 "back_end": self.backend_repo_dir,
                 "front_end": self.frontend_repo_dir,
             },
         )
         self._execute("nginx -t && service nginx restart")
+        for startupcmd in self.startup_cmds:
+            startupcmd.start()
 
     @property
     def startup_cmds(self):
