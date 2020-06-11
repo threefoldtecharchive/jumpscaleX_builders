619d9be23aa0a3385363ffee9cb8efc12a887058
Rana Tarek Hassan <tarekr@codescalers.com>
2020-06-11 14:55:43 +0200

    Use convention builder functions in grafana admin builder

--- JumpscaleBuildersCommunity/monitoring/BuilderGrafanaAdmin.py
+++ JumpscaleBuildersCommunity/monitoring/BuilderGrafanaAdmin.py
@@ -8,9 +8,9 @@ class BuilderGrafanaAdmin(j.baseclasses.builder):
     __jslocation__ = "j.builders.monitoring.grafana_admin"
 
     def _init(self, **kwargs):
-        j.core.tools.dir_ensure(self.DIR_BUILD)
+        self._dir_ensure(self.DIR_BUILD)
         self.version = "7.0.3"
-        self.DIR_BASE_GRAFANA = "/sandbox/grafana"
+        self.DIR_BASE_GRAFANA = f"{j.core.dirs.BASEDIR}/grafana"
         self.DIR_INSTALL = self.DIR_BUILD + f"/grafana-{self.version}"
 
     @builder_method()
@@ -25,9 +25,9 @@ class BuilderGrafanaAdmin(j.baseclasses.builder):
 
         set -ex
 
-        rm -rf /sandbox/grafana
         rm {DIR_BIN}/grafana-server
         rm {DIR_BIN}/grafana-cli
+        rm -rf /sandbox/grafana
         rm -rf {DIR_BUILD}
         """
         self._execute(C)
@@ -41,32 +41,28 @@ class BuilderGrafanaAdmin(j.baseclasses.builder):
     def build(self, reset=False):
 
         if j.core.platformtype.myplatform.platform_is_ubuntu:
-            j.core.tools.dir_ensure(self.DIR_BUILD)
+            self._dir_ensure(self.DIR_BUILD)
             C = f"""
             cd {self.DIR_BUILD}
             wget https://dl.grafana.com/oss/release/grafana-{self.version}.linux-amd64.tar.gz
             tar -zxvf grafana-{self.version}.linux-amd64.tar.gz
             """
-            j.sal.process.execute(C)
+            self._execute(C)
         else:
             raise j.exceptions.Base("platform not supported")
 
     @builder_method()
     def install(self, reset=False):
-        j.core.tools.dir_ensure(self.DIR_BUILD)
-        j.builders.tools.copyTree(
-            f"{self.DIR_BUILD}/grafana-{self.version}", dest=self.DIR_BASE_GRAFANA, recursive=True
-        )
+        self._dir_ensure(self.DIR_BUILD)
+        self._copy(f"{self.DIR_BUILD}/grafana-{self.version}", self.DIR_BASE_GRAFANA)
 
-        j.builders.tools.file_copy(f"{self.DIR_INSTALL}/bin/grafana-server", "{DIR_BIN}")
-        j.builders.tools.file_copy(f"{self.DIR_INSTALL}/bin/grafana-cli", "{DIR_BIN}")
-        j.builders.tools.file_copy(
-            f"{self.DIR_INSTALL}/conf/defaults.ini", f"{self.DIR_BASE_GRAFANA}/conf/grafana_cfg.ini"
-        )
+        self._copy(f"{self.DIR_INSTALL}/bin/grafana-server", "{DIR_BIN}")
+        self._copy(f"{self.DIR_INSTALL}/bin/grafana-cli", "{DIR_BIN}")
+        self._copy(f"{self.DIR_INSTALL}/conf/defaults.ini", f"{self.DIR_BASE_GRAFANA}/conf/grafana_cfg.ini")
 
         C = f"""
         sed -i 's@http_port = 3000@http_port = 3005@g' {self.DIR_BASE_GRAFANA}/conf/grafana_cfg.ini
         sed -i 's@root_url = %(protocol)s://%(domain)s:%(http_port)s/@root_url = %(protocol)s://%(domain)s:%(http_port)s/grafana/@g' {self.DIR_BASE_GRAFANA}/conf/grafana_cfg.ini
         sed -i 's@allow_embedding = false@allow_embedding = true@g' {self.DIR_BASE_GRAFANA}/conf/grafana_cfg.ini
         """
-        j.sal.process.execute(C)
+        self._execute(C)
